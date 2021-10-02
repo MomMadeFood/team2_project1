957deb65c77d2293b40bffde663c4aead071ade7
wandukong <seungwan37@naver.com>
2021-10-02 14:06:25 +0900

    Update ProductDTO.java

--- src/main/java/com/mycompany/webapp/dto/product/ProductDTO.java
+++ src/main/java/com/mycompany/webapp/dto/product/ProductDTO.java
@@ -1,15 +1,21 @@
 package com.mycompany.webapp.dto.product;
-
-import java.util.List;
-
 import lombok.Data;
 
 @Data
 public class ProductDTO {
+	//Product
 	private String productNo;
 	private String name;
 	private int price;
 	private String content;
 	private String brand;
-	private List<ProductDetailDTO> list;
+
+	//Product_Detail
+	private String productDetailNo;
+	private String withProduct;
+	private String img1;
+	private String img2;
+	private String img3;
+	private String colorCode;
+	private String colorChip;
 }
