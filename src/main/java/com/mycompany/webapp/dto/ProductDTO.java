19eeaff023946c03f8089c989e15e9a648bdde2d
Chenjae <injea95@naver.com>
2021-10-02 10:20:54 +0900

    [refactor]  #114: ProductDTO, CouponDTO 수정

--- src/main/java/com/mycompany/webapp/dto/ProductDTO.java
+++ src/main/java/com/mycompany/webapp/dto/ProductDTO.java
@@ -4,9 +4,19 @@ import lombok.Data;
 
 @Data
 public class ProductDTO {
+	//Product
 	private String productNo;
 	private String name;
 	private int price;
 	private String content;
 	private String brand;
+	
+	//Product_Detail
+	private String productDetailNo;
+	private String withProduct;
+	private String img1;
+	private String img2;
+	private String img3;
+	private String colorCode;
+	private String colorChip;
 }
