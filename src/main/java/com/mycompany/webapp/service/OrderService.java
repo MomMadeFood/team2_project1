f4515260e61533a5b7957e87e03143bc49135288
kim jungwoo <kjw970103@gmail.com>
2021-10-07 19:10:28 +0900

    [Feat] #285: orderForm 프로그래밍적 transaction 적용하기

--- src/main/java/com/mycompany/webapp/service/OrderService.java
+++ src/main/java/com/mycompany/webapp/service/OrderService.java
@@ -12,7 +12,11 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.mycompany.webapp.controller.OrderController;
 import com.mycompany.webapp.dao.CardDAO;
@@ -21,15 +25,18 @@ import com.mycompany.webapp.dao.MOrderDAO;
 import com.mycompany.webapp.dao.MemberDAO;
 import com.mycompany.webapp.dao.OrderDetailDAO;
 import com.mycompany.webapp.dao.PaymentDAO;
+import com.mycompany.webapp.dao.PointDAO;
 import com.mycompany.webapp.dao.ProductDAO;
 import com.mycompany.webapp.dao.ProductDetailDAO;
 import com.mycompany.webapp.dao.StockDAO;
 import com.mycompany.webapp.dto.CartDTO;
+
 import com.mycompany.webapp.dto.MOrderDTO;
 import com.mycompany.webapp.dto.MemberDTO;
 import com.mycompany.webapp.dto.OrderDetailDTO;
 import com.mycompany.webapp.dto.OrderListDTO;
 import com.mycompany.webapp.dto.PaymentDTO;
+import com.mycompany.webapp.dto.PointDTO;
 import com.mycompany.webapp.dto.StockDTO;
 import com.mycompany.webapp.dto.product.ProductDTO;
 import com.mycompany.webapp.exception.DeleteOrderException;
@@ -39,6 +46,9 @@ import com.mycompany.webapp.exception.NotEnoughtStockException;
 public class OrderService {
 
 	private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+	
+	@Resource
+	private TransactionTemplate transactionTemplate;
 	
 	@Resource
 	private OrderDetailDAO orderDetailDAO;
@@ -59,7 +69,10 @@ public class OrderService {
 	private MemberDAO memberDAO;
 	
 	@Resource
-	CartDAO cartDAO;
+	private CartDAO cartDAO;
+	
+	@Resource
+	private PointDAO pointDAO;
 	
 	public enum OrderResult{
 		SUCCESS,
@@ -142,102 +155,127 @@ public class OrderService {
 	}
 
 	
-	@Transactional
+
 	public Map<String,String> insertMOrder(MOrderDTO mOrderDTO) {
 		Map<String,String> resultMap = new HashMap();
-		try {
-			
-			List<OrderDetailDTO> detailList = mOrderDTO.getDetailList();
-			List<PaymentDTO> paymentList = mOrderDTO.getPaymentList();
-			
-			
-			SimpleDateFormat format1 = new SimpleDateFormat( "yyyyMMdd");
+		logger.info("실행");
+		
+		Map<String,String> result = transactionTemplate.execute(new TransactionCallback<Map<String,String> >() {
+			@Override
+			public Map<String,String> doInTransaction(TransactionStatus status) {
+				try {
+					List<OrderDetailDTO> detailList = mOrderDTO.getDetailList();
+					List<PaymentDTO> paymentList = mOrderDTO.getPaymentList();
 					
+					
+					SimpleDateFormat format1 = new SimpleDateFormat( "yyyyMMdd");
+							
 
-			
-			// 상품의 재고를 확인하는 동시에 감소시키는 로직
-			logger.info("삽입 시작1");
-			for(OrderDetailDTO orderDetailDTO:  detailList) {
-				ProductDTO productDTO = new ProductDTO();
-				productDTO.setProductDetailNo(orderDetailDTO.getProductDetailNo());
-				productDTO.setProductNo(productDTO.getProductDetailNo().substring(0, productDTO.getProductDetailNo().length()-3));
-				ProductDTO product = productDAO.selectProductByProduct(productDTO);
-				int updateResult = stockDAO.updateStockByODIdSize(orderDetailDTO);
-				
-				if(updateResult==0) {
-					 logger.info(orderDetailDTO.toString());
-					 throw new NotEnoughtStockException(product.getName());
+					
+					// 상품의 재고를 확인하는 동시에 감소시키는 로직
+					logger.info("삽입 시작1");
+					for(OrderDetailDTO orderDetailDTO:  detailList) {
+						ProductDTO productDTO = new ProductDTO();
+						productDTO.setProductDetailNo(orderDetailDTO.getProductDetailNo());
+						productDTO.setProductNo(productDTO.getProductDetailNo().substring(0, productDTO.getProductDetailNo().length()-3));
+						ProductDTO product = productDAO.selectProductByProduct(productDTO);
+						int updateResult = stockDAO.updateStockByODIdSize(orderDetailDTO);
+						
+						if(updateResult==0) {
+							 logger.info(orderDetailDTO.toString());
+							 throw new NotEnoughtStockException(product.getName());
+						}
+						
+						logger.info("개수: "+updateResult);
+					}
+					
+					
+					// 주문 정보를 삽입하는 코드
+					int cnt = (10000000+mOrderDAO.selectMOrderCount())%100000000;
+					String conv = Integer.toString(cnt);
+					Date time = new Date();
+					String time1 = format1.format(time);
+					String orderNo = time1+"P"+conv;
+					System.out.println(orderNo);
+					mOrderDTO.setOrderNo(orderNo);
+					
+					logger.info("삽입 시작2");
+					mOrderDAO.insertMOrder(mOrderDTO);
+					
+					logger.info("----");
+					
+					logger.info("삽입 시작3");
+					// 주문 상세정보를 삽입하는 코드 
+					// 상세정보를 삽입하는 동시에 카트에서 해당 정보를 삭제한다.
+					for(OrderDetailDTO orderDetailDTO:  detailList) {
+						orderDetailDTO.setOrderNo(orderNo);
+						int orderDetailInsertResult = orderDetailDAO.insertOrderDetail(orderDetailDTO);
+						
+						CartDTO cartDTO = new CartDTO();
+						cartDTO.setMemberId(mOrderDTO.getMemberId());
+						cartDTO.setProductDetailNo(orderDetailDTO.getProductDetailNo());
+						cartDTO.setPsize(orderDetailDTO.getPsize());
+						
+						cartDAO.deleteCart(cartDTO);
+						logger.info("개수: "+orderDetailInsertResult);
+					}
+					
+					logger.info("삽입 시작4");
+					
+					
+					// 결제타입을 삽입하는 코드 포인트를 사용했다면 포인트도 차감함
+					for(PaymentDTO paymentDTO:  paymentList) {
+						paymentDTO.setOrderNo(orderNo);
+						int paymentInsertResult = paymentDAO.insertPayment(paymentDTO);
+						
+						if(paymentDTO.getPaymentType().equals("포인트")) {
+							MemberDTO memberDTO = new MemberDTO();
+							memberDTO.setId(mOrderDTO.getMemberId());
+							memberDTO.setPoint(paymentDTO.getPrice());
+							memberDAO.updatePointById(memberDTO);
+						}
+						logger.info("개수: "+paymentInsertResult);
+					}
+					
+					// 결제가 완료됐으면 신용카드의 경우 즉시 포인트를 적립할 수 있게 함
+					if(paymentList.get(0).getPaymentType().equals("신용카드")) {
+						int pointCnt = (10000000+pointDAO.selectCounts())%100000000;
+						for(OrderDetailDTO orderDetailDTO:  detailList) {
+							PointDTO pointDTO = new PointDTO();
+							pointDTO.setAmount((int)(orderDetailDTO.getPrice()/100));
+							pointDTO.setRemain(pointDTO.getAmount());
+							pointDTO.setMemberId(mOrderDTO.getMemberId());
+							pointDTO.setName("구매적립");
+							pointDTO.setState(0);
+							conv = Integer.toString(pointCnt);
+							String pointNo = time1+"P"+conv;
+							pointDTO.setPointNo(pointNo);
+							int pointResult = pointDAO.insertPoint(pointDTO);
+							if(pointResult==0)
+								throw new Exception();
+							pointCnt++;
+						}	
+					}
+					resultMap.put("result","success");
+					resultMap.put("orderNo",mOrderDTO.getOrderNo());
+					return resultMap;
+				}catch (NotEnoughtStockException e) {
+					logger.info(e.getMessage());
+					status.setRollbackOnly();
+					resultMap.put("result","soldout");
+					resultMap.put("productName",e.getMessage());
+					return resultMap;
+				}catch(Exception e) {
+					status.setRollbackOnly();
+					e.printStackTrace();
+					resultMap.put("result","fail");
+					return resultMap;
 				}
 				
-				logger.info("개수: "+updateResult);
 			}
-			
-			
-			// 주문 정보를 삽입하는 코드
-			int cnt = (10000000+mOrderDAO.selectMOrderCount())%100000000;
-			String conv = Integer.toString(cnt);
-			Date time = new Date();
-			String time1 = format1.format(time);
-			String orderNo = time1+"P"+conv;
-			System.out.println(orderNo);
-			mOrderDTO.setOrderNo(orderNo);
-			
-			logger.info("삽입 시작2");
-			mOrderDAO.insertMOrder(mOrderDTO);
-			
-			logger.info("----");
-			
-			logger.info("삽입 시작3");
-			// 주문 상세정보를 삽입하는 코드 
-			// 상세정보를 삽입하는 동시에 카트에서 해당 정보를 삭제한다.
-			for(OrderDetailDTO orderDetailDTO:  detailList) {
-				orderDetailDTO.setOrderNo(orderNo);
-				int orderDetailInsertResult = orderDetailDAO.insertOrderDetail(orderDetailDTO);
-				
-				CartDTO cartDTO = new CartDTO();
-				cartDTO.setMemberId(mOrderDTO.getMemberId());
-				cartDTO.setProductDetailNo(orderDetailDTO.getProductDetailNo());
-				cartDTO.setPsize(orderDetailDTO.getPsize());
-				
-				cartDAO.deleteCart(cartDTO);
-				
-				logger.info("개수: "+orderDetailInsertResult);
-			}
-			
-			logger.info("삽입 시작4");
-			
-			
-			// 결제타입을 삽입하는 코드 포인트를 사용했다면 포인트도 차감함
-			for(PaymentDTO paymentDTO:  paymentList) {
-				paymentDTO.setOrderNo(orderNo);
-				int paymentInsertResult = paymentDAO.insertPayment(paymentDTO);
-				
-				if(paymentDTO.getPaymentType().equals("포인트")) {
-					MemberDTO memberDTO = new MemberDTO();
-					memberDTO.setId(mOrderDTO.getMemberId());
-					memberDTO.setPoint(paymentDTO.getPrice());
-					memberDAO.updatePointById(memberDTO);
-				}
-				logger.info("개수: "+paymentInsertResult);
-			}
-			
-			
-
-		}
-		catch (NotEnoughtStockException e) {
-			logger.info(e.getMessage());
-			resultMap.put("result","soldout");
-			resultMap.put("productName",e.getMessage());
-			return resultMap;
-		}catch(Exception e) {
-			resultMap.put("result","fail");
-			return resultMap;
-		}
+		});
 		
-		resultMap.put("result","success");
-		resultMap.put("orderNo",mOrderDTO.getOrderNo());
-		
-		return resultMap;
+		return result;
 	}
 	
 	
