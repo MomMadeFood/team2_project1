b39a386bcbcdbd1c3cb620c514c8adb552c35587
Chenjae <injea95@naver.com>
2021-10-07 11:01:28 +0900

    [Fix] #264: 상세상품 페이지에서 카트 담을 때 오류

--- src/main/java/com/mycompany/webapp/controller/CartController.java
+++ src/main/java/com/mycompany/webapp/controller/CartController.java
@@ -4,13 +4,13 @@ import java.security.Principal;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,8 +33,8 @@ import com.mycompany.webapp.service.ProductDetailService;
 public class CartController {
 	private static final Logger logger = LoggerFactory.getLogger(CartController.class);
 	
-	@Autowired private CartService cartService;
-	@Autowired private ProductDetailService productDetailService;
+	@Resource private CartService cartService;
+	@Resource private ProductDetailService productDetailService;
 	
 	@RequestMapping("")
 	public String cart(
--- src/main/java/com/mycompany/webapp/controller/MemberController.java
+++ src/main/java/com/mycompany/webapp/controller/MemberController.java
@@ -1,11 +1,7 @@
 package com.mycompany.webapp.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
